f606de211c304ee9d648fffdd00fa8b21698b713
PRAKSH-DATASIRPI <prakash.m@datasirpi.com>
2024-01-01 09:44:31 +0530

    operator done

---
+++ operator.java
@@ -0,0 +1,44 @@
+public class operator {
+
+    public static void main(String[]args){
+        int a=1;
+        int b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        // Assignment op
+        int i= 2;
+        System.out.println(i);
+        int ad=7;
+        ad+=3;
+         System.out.println(ad);
+        ad-=2;
+         System.out.println(ad);
+        ad*=3;
+        System.out.println(ad);
+        ad/=2;
+         System.out.println(ad);
+        // comparsion op
+        int k=17;
+        int l=23;
+        System.out.println(k<l);
+        System.out.println(k==l);
+        System.out.println(k>l);
+        System.out.println(k!=l);
+        System.out.println(k<=l);
+        System.out.println(k>=l);
+        // logical op
+        int and = 3;
+        int or=4;
+        System.out.println(and>or&&and<or);
+        System.out.println(and>or||and<or);
+        System.out.println(!(and>or&&and<or));
+        
+
+
+
+    }
+    
+}
