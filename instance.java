b118eefb57be5ba7fad6bd9da5e8a2c80410aabe
PRAKSH-DATASIRPI <prakash.m@datasirpi.com>
2024-01-01 17:15:35 +0530

    instance done

---
+++ instance.java
@@ -0,0 +1,17 @@
+public class instance {
+    int c;
+       
+    void add(int a,int b){
+        int c =3;
+        System.out.println(a+b+c);
+    }
+
+    public static void main(String[] args) {
+        instance obj =new instance();
+        obj.add(2,3);
+
+    }
+
+
+
+}
