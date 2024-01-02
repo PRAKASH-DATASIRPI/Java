a860f16e336d8bf43de67ceaa193cbe03e2eaee8
PRAKSH-DATASIRPI <prakash.m@datasirpi.com>
2024-01-02 17:40:23 +0530

    staticblockdone

---
+++ multistat.java
@@ -0,0 +1,20 @@
+public class multistat {
+   static  String name;
+   static  String naam;
+   static{
+    System.out.println("fst");
+    name="prakash";
+    naam="prakash";
+}
+static{
+    System.out.println("scnd");
+    name="murugan";
+    naam="murugan";
+}
+public static void main(String[] args) {
+    System.out.println(name);
+    System.out.println(naam);
+}
+
+    
+}
---
+++ stat.java
@@ -0,0 +1,13 @@
+public class stat {
+     static String name;
+     static String naam;
+    static{
+        name="prakash";
+        naam="prakash";
+    }
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(naam);
+    }
+
+}
